b8e4c9b8014cff207440f2c6b6ed84180e387ea1
kathlynee <ksarahfania@student.ciputra.ac.id>
2022-09-18 18:15:16 +0700

    Initial commit

---
+++ app/src/main/java/com/example/week1_0706012110057/model/UserData.kt
@@ -0,0 +1,7 @@
+package com.example.week1_0706012110057.model
+
+data class UserData (
+    var userName:String,
+    var userType:String,
+    var userMb:String
+)
---
+++ app/src/main/java/com/example/week1_0706012110057/view/UserAdapter.kt
@@ -0,0 +1,116 @@
+package com.example.week1_0706012110057.view
+
+import android.app.AlertDialog
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.*
+import androidx.recyclerview.widget.RecyclerView
+import com.example.week1_0706012110057.R
+import com.example.week1_0706012110057.model.UserData
+
+class UserAdapter(val c:Context,val userList:ArrayList<UserData>):RecyclerView.Adapter<UserAdapter.UserViewHolder>()
+{
+
+
+
+    inner class UserViewHolder(val v:View):RecyclerView.ViewHolder(v){
+        var name:TextView
+        var type:TextView
+        var mbNum:TextView
+        var mMenus:ImageView
+
+        init {
+            name = v.findViewById<TextView>(R.id.mTitle)
+            type = v.findViewById<TextView>(R.id.mTypes)
+            mbNum = v.findViewById<TextView>(R.id.mSubTitle)
+            mMenus = v.findViewById(R.id.mMenus)
+            mMenus.setOnClickListener { popupMenus(it) }
+        }
+
+        private fun popupMenus(v:View) {
+            val position = userList[adapterPosition]
+            val popupMenus = PopupMenu(c,v)
+            popupMenus.inflate(R.menu.show_menu)
+            popupMenus.setOnMenuItemClickListener {
+                when(it.itemId){
+                    R.id.editText->{
+                        val v = LayoutInflater.from(c).inflate(R.layout.add_item,null)
+                        val name = v.findViewById<EditText>(R.id.userName)
+                        val type = v.findViewById<EditText>(R.id.userType)
+                        val number = v.findViewById<EditText>(R.id.userNo)
+                        AlertDialog.Builder(c)
+                            .setView(v)
+                            .setPositiveButton("Ok"){
+                                    dialog,_->
+                                position.userName = name.text.toString()
+                                position.userType = type.text.toString()
+                                position.userMb = number.text.toString()
+                                notifyDataSetChanged()
+                                Toast.makeText(c,"Animal Information is Edited",Toast.LENGTH_SHORT).show()
+                                dialog.dismiss()
+
+                            }
+                            .setNegativeButton("Cancel"){
+                                    dialog,_->
+                                dialog.dismiss()
+
+                            }
+                            .create()
+                            .show()
+
+                        true
+                    }
+                    R.id.delete->{
+                        /**set delete*/
+                        AlertDialog.Builder(c)
+                            .setTitle("Delete")
+                            .setIcon(R.drawable.ic_warning)
+                            .setMessage("Are you sure delete this Animal?")
+                            .setPositiveButton("Yes"){
+                                    dialog,_->
+                                userList.removeAt(adapterPosition)
+                                notifyDataSetChanged()
+                                Toast.makeText(c,"Deleted this Animal",Toast.LENGTH_SHORT).show()
+                                dialog.dismiss()
+                            }
+                            .setNegativeButton("No"){
+                                    dialog,_->
+                                dialog.dismiss()
+                            }
+                            .create()
+                            .show()
+
+                        true
+                    }
+                    else-> true
+                }
+
+            }
+            popupMenus.show()
+            val popup = PopupMenu::class.java.getDeclaredField("mPopup")
+            popup.isAccessible = true
+            val menu = popup.get(popupMenus)
+            menu.javaClass.getDeclaredMethod("setForceShowIcon",Boolean::class.java)
+                .invoke(menu,true)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        val v  = inflater.inflate(R.layout.list_item,parent,false)
+        return UserViewHolder(v)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val newList = userList[position]
+        holder.name.text = newList.userName
+        holder.type.text = newList.userType
+        holder.mbNum.text = newList.userMb
+    }
+
+    override fun getItemCount(): Int {
+        return  userList.size
+    }
+}
